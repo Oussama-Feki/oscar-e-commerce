b05699a4ed85e5e006ff18aa79bd84fb985cfca0
Alice <wadiimakhlouf95@email.com>
2021-07-07 02:44:37 +0100

    changed

--- frobshop/frobshop/settings.py
+++ frobshop/frobshop/settings.py
@@ -181,9 +181,11 @@ ACCOUNT_EMAIL_VERIFICATION = "none"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
