13cdeabc2fa5bbcb12efc07ebeb2836e1a8a97ab
Alice <wadiimakhlouf95@email.com>
2021-07-13 22:26:36 +0100

    changed

--- frobshop/frobshop/settings.py
+++ frobshop/frobshop/settings.py
@@ -168,7 +168,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
@@ -180,13 +180,12 @@ USE_TZ = True
 
 # Lists of languages site supports.
 LANGUAGES = (
-    ('en', _('English')),
+#    ('en', _('English')),
     ('fr', _('French')),
 )
  
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR,'locale'),
-]
+
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
